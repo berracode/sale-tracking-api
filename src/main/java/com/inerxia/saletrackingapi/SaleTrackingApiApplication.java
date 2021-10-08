ec1a15c9a02462d75c89295d1ffaf2e6c1fc76d3
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-10-07 22:35:30 -0500

    First commit

---
+++ src/main/java/com/inerxia/saletrackingapi/SaleTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.inerxia.saletrackingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleTrackingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleTrackingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inerxia/saletrackingapi/SaleTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inerxia.saletrackingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleTrackingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
