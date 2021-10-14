b697ad575cc4ba61450a6740fa98f7f03ebe7725
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-10-14 09:03:07 -0500

    se crea buscar por nombre en customer

--- src/main/java/com/inerxia/saletrackingapi/controller/CustomerController.java
+++ src/main/java/com/inerxia/saletrackingapi/controller/CustomerController.java
@@ -1,8 +1,19 @@
 package com.inerxia.saletrackingapi.controller;
 
+import com.inerxia.saletrackingapi.dto.CustomerDto;
+import com.inerxia.saletrackingapi.dto.ProviderDto;
 import com.inerxia.saletrackingapi.facade.CustomerFacade;
+import com.inerxia.saletrackingapi.util.StandardResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -11,5 +22,22 @@ public class CustomerController {
 
     public CustomerController(CustomerFacade customerFacade) {
         this.customerFacade = customerFacade;
+    }
+
+
+    @GetMapping({"/get-by-name"})
+    @ApiOperation(value = "Find customer by name", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "La petición fue procesada con éxito"),
+            @ApiResponse(code = 400, message = "La petición es inválida"),
+            @ApiResponse(code = 500, message = "Error del servidor al procesar la respuesta"),
+    })
+    public ResponseEntity<StandardResponse<List<CustomerDto>>> findByName(
+            @RequestParam(name = "aname",defaultValue = "",required = false) String name){
+
+        List<CustomerDto> customerDtoList = customerFacade.findByName(name);
+        return ResponseEntity.ok(new StandardResponse<>(
+                StandardResponse.StatusStandardResponse.OK,
+                customerDtoList));
     }
 }
--- src/main/java/com/inerxia/saletrackingapi/facade/CustomerFacade.java
+++ src/main/java/com/inerxia/saletrackingapi/facade/CustomerFacade.java
@@ -1,9 +1,12 @@
 package com.inerxia.saletrackingapi.facade;
 
+import com.inerxia.saletrackingapi.dto.CustomerDto;
 import com.inerxia.saletrackingapi.mapper.CustomerMapper;
 import com.inerxia.saletrackingapi.service.CustomerService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -14,5 +17,9 @@ public class CustomerFacade {
     public CustomerFacade(CustomerService customerService, CustomerMapper customerMapper) {
         this.customerService = customerService;
         this.customerMapper = customerMapper;
+    }
+
+    public List<CustomerDto> findByName(String name){
+        return customerMapper.toDto(customerService.findByName(name));
     }
 }
